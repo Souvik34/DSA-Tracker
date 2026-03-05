f350132c1a9420fb8aa0830830313500e314557c
Souvik Sural <98397984+Souvik34@users.noreply.github.com>
2026-03-05 18:28:47 +0530

    Fix for Postgres

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import { Pool } from "pg";
 
 console.log("ENV DB_PORT:", process.env.DB_PORT);
@@ -6,7 +9,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   port: Number(process.env.DB_PORT),
 });
 

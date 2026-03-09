987b042c9696fcf9a030acb7731a5eae9e0bbcce
Souvik Sural <98397984+Souvik34@users.noreply.github.com>
2026-03-09 17:50:28 +0530

    feat: implement problems repository with CRUD operations

---
+++ server/src/modules/problems/problems.repository.js
@@ -0,0 +1,24 @@
+import pool from "../../db/db";
+
+export const getAllProblems = async () => {
+  const res = await pool.query("SELECT * FROM problems ORDER BY created_at DESC");
+    return res.rows;
+};
+
+export const getProblemById = async (id) => {
+  const res = await pool.query("SELECT * FROM problems WHERE id = $1", [id]);
+  return res.rows[0];
+};
+
+export const createProblem = async ({
+  title,
+  question_link,
+  difficulty,
+  topic,
+  tags,
+  platform
+}) => {
+    const res = await pool.query(`INSERT INTO problems (title, question_link, difficulty, topic, tags, platform) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
+    [title, question_link, difficulty, topic, tags, platform]);
+    return res.rows[0];
+}

d562c2f636b0940c22c3acb837026920e7ff6827
Souvik Sural <98397984+Souvik34@users.noreply.github.com>
2026-03-09 18:35:22 +0530

    feat: enhance getAllProblems method to support pagination and filtering

--- server/src/modules/problems/problems.controller.js
+++ server/src/modules/problems/problems.controller.js
@@ -1,37 +1,44 @@
+import { success } from "zod";
 import * as problemService from "./problems.service.js";
 
 export const getAllProblems = async (req, res) => {
-    try{
-        const problems = await problemService.getAllProblems();
-        res.status(200).json(problems);
-
-    }
-    catch(err){
-        res.status(500).json({ message: "Internal Server Error" });
-    }
+  try {
+    const { difficulty, topic, page = 1, limit = 50 } = req.query;
+    const problems = await problemService.getAllProblems({
+      page: Number(page),
+      limit: Number(limit),
+      difficulty: difficulty ? difficulty.split(",") : undefined,
+      topic: topic ? topic.split(",") : undefined,
+    });
+    res.status(200).json({
+      success: true,
+      page: Number(page),
+      limit: Number(limit),
+      data: problems,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
-
 export const getProblemById = async (req, res) => {
-    try{
-        const { id } = req.params;
-        const problem = await problemService.getProblemById(id);
-        if (!problem) {
-            return res.status(404).json({ message: "Problem not found" });
-        }
-        res.status(200).json(problem);
+  try {
+    const { id } = req.params;
+    const problem = await problemService.getProblemById(id);
+    if (!problem) {
+      return res.status(404).json({ message: "Problem not found" });
     }
-    catch(err){
-        res.status(500).json({ message: "Internal Server Error" });
-    }
+    res.status(200).json(problem);
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 export const createProblem = async (req, res) => {
-    try{
-        const problem = await problemService.createProblem(req.body);
-        res.status(201).json(problem);
-    }   
-    catch(err){
-        res.status(500).json({ message: "Internal Server Error" });
-    }   
+  try {
+    const problem = await problemService.createProblem(req.body);
+    res.status(201).json(problem);
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };

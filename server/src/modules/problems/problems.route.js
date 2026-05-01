49de9052e571562a0b0063acadc78ace54bd4cea
Souvik Sural <98397984+Souvik34@users.noreply.github.com>
2026-05-01 18:03:20 +0530

    feat: add route for marking problems as solved

--- server/src/modules/problems/problems.route.js
+++ server/src/modules/problems/problems.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { getAllProblems,getProblemById, createProblem} from "./problems.controller.js";
-
+import { markProblemSolved } from "./problems.controller.js";
 const router = express.Router();
 
 router.get("/", getAllProblems);
 router.get("/:id", getProblemById);
 router.post("/", createProblem);
+router.post("/solve/:userId", markProblemSolved);
 
 export default router;

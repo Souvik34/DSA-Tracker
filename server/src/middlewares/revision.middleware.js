ee719cf282255b2114a659f01a59fcc842876a5c
Souvik Sural <98397984+Souvik34@users.noreply.github.com>
2026-06-28 19:28:05 +0530

    feat: add revision middleware to enforce due revisions check before solving problems

---
+++ server/src/middlewares/revision.middleware.js
@@ -0,0 +1,36 @@
+import { getDueRevisionsService } from "../modules/revision/revision.service.js";
+
+export const revisionMiddleware = async (req, res, next) => {
+  try {
+    const userId = Number(req.params.userId);
+
+    if (!userId || isNaN(userId)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid userId",
+      });
+    }
+
+    const dueRevisions = await getDueRevisionsService(userId);
+
+    if (dueRevisions.length >= 2) {
+      return res.status(403).json({
+        success: false,
+        blocked: true,
+        message:
+          "You have pending revisions. Complete them before solving new problems.",
+        totalDue: dueRevisions.length,
+        revisions: dueRevisions,
+      });
+    }
+
+    next();
+  } catch (err) {
+    console.error("Revision Middleware:", err);
+
+    return res.status(500).json({
+      success: false,
+      message: "Revision check failed",
+    });
+  }
+};
--- server/src/modules/problems/problems.route.js
+++ server/src/modules/problems/problems.route.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { getAllProblems,getProblemById, createProblem} from "./problems.controller.js";
 import { markProblemSolved } from "./problems.controller.js";
+import { revisionMiddleware } from "../../middlewares/revision.middleware.js";
 const router = express.Router();
 
 router.get("/", getAllProblems);
 router.get("/:id", getProblemById);
 router.post("/", createProblem);
-router.post("/solve/:userId", markProblemSolved);
-
+router.post(
+  "/solve/:userId",
+  revisionMiddleware,
+  markProblemSolved
+);
 export default router;

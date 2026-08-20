623d80580df3e19fd4e9aa1ee4afd266010cd57a
Souvik Sural <98397984+Souvik34@users.noreply.github.com>
2026-08-21 02:32:39 +0530

    fix: update base URL in api and socket files to use the correct server address

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { toast } from "sonner";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+ baseURL: "http://15.252.139.152:5000/api/v1",
   withCredentials: false,
   timeout: 120000,
 });
